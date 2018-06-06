23b5253f39605f4fe5de524cc7a6d8e918115026
123 <123@gmail.com>
2018-06-06 16:38:30 +0200

    some changes

---
+++ Circulos/Assets/UnityChan/Scripts/Translate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour {
+
+    public Transform ch;
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        //ch.position = new Vector3(0, ch.position.y * speed * Time.deltaTime, 0);
+        ch.transform.Translate(Vector3.up * speed * Time.deltaTime);
+            if(ch.position.y >= 0) speed = 0f;
+	}
+}
